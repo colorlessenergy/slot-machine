52559106c6fa54c6c25ebe0c14387cc3315dc4ea
colorlessenergy <brianmunozdev@gmail.com>
2020-05-27 19:21:19 -0700

    add logic for rolling 3 reels, display on screen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Slots from './components/Slots';
+
+function App() {
+  return (
+    <div className="App">
+      <Slots />
+    </div>
+  );
+}
+
+export default App;
